d790930f2d47281f444de40f577b701ceac5775c
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2026-01-04 00:12:45 +0200

    Fix missing using directive in DbInitializer

--- Ehjoz.Infrastructure/Data/DbInitializer.cs
+++ Ehjoz.Infrastructure/Data/DbInitializer.cs
@@ -1,6 +1,7 @@
 using EhjozProject.Domain.Models.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EhjozProject.Infrastructure.Data
 {
