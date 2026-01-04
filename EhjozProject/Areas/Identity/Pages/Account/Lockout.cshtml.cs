b74fefe7af463d7de01a5dd004e771c3b2527c90
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2026-01-04 03:24:04 +0200

    Customize lockout page message

---
+++ EhjozProject/Areas/Identity/Pages/Account/Lockout.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EhjozProject.Web.Areas.Identity.Pages.Account
+{
+    public class LockoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
+
